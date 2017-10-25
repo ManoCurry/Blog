0edc9f40a5a3341bce14cd2101b0f7966ab540e7
Jiseung Roh <jiseung.roh@gmail.com>
2017-10-25 22:09:56 +0900

    Try to extract css file from NextJS, ended up failing

---
+++ next.config.js
@@ -0,0 +1,49 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  distDir: './.next',
+  webpack: (config, { dev }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      loader: 'emit-file-loader',
+      options: {
+        name: 'dist/[path][name].[ext]'
+      }
+    })
+    if (!dev) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                modules: false,
+                url: true,
+                sourceMap: false,
+                minimize: true,
+                localIdentName: false
+                  ? '[name]-[local]-[hash:base64:5]'
+                  : '[hash:base64:5]'
+              }
+            },
+            { loader: 'postcss-loader' }
+          ]
+        })
+      })
+
+      config.plugins.push(new ExtractTextPlugin('static/styles/style.css'))
+    } else {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: [
+          { loader: 'raw-loader' },
+          { loader: 'postcss-loader' }
+        ]
+      })
+    }
+
+    return config
+  }
+};
