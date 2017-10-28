17cdd5af9b06e53b9b3c6817f27382117fab13c1
Jiseung Roh <jiseung.roh@gmail.com>
2017-10-28 21:47:11 +0900

    Remove unnecessary element

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import AppLayout from '../components/layouts/AppLayout'
-import Textarea from '../components/Textarea'
 
 const Index = () => (
   <AppLayout>
     <h2>index</h2>
-    <Textarea />
   </AppLayout>
 )
 
