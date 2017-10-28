60c025d40c1da8312846d9baa0a584af1383a16c
Jiseung Roh <jiseung.roh@gmail.com>
2017-10-28 21:44:46 +0900

    Wrap MarkdownRenderer with styles

--- components/MarkdownRenderer.js
+++ components/MarkdownRenderer.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import remark from 'remark'
 import reactRenderer from 'remark-react'
+import NoteLayout from '../components/layouts/NoteLayout'
 
 const MarkdownRenderer = ({
   source
-}) => remark()
-  .use(reactRenderer)
-  .processSync(source)
-  .contents
+}) => (
+    <article className='markdown'>
+      {
+        remark()
+          .use(reactRenderer)
+          .processSync(source)
+          .contents
+      }
+      <style jsx global>{`
+        .markdown h1 {
+          font-size: 53px;
+          line-height: 1.2;
+          margin-bottom: 40px;
+        }
+
+        .markdown p {
+          font-size: 16px;
+          line-height: 1.6875;
+          margin-bottom: 27px;
+        }
+      `}</style>
+    </article>
+)
 
 export default MarkdownRenderer
