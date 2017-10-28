cc61fe1cf74a2846c3b89bedfbd9fe47420f5c8c
Jiseung Roh <jiseung.roh@gmail.com>
2017-10-28 18:41:56 +0900

    Add layout components

--- components/layouts/SectionLayout.js
+++ components/layouts/SectionLayout.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
-import sectionLayoutStyle from '../styles/sectionLayoutStyle'
 
 const SectionLayout = (props) => (
   <section className='section-layout'>
-    <div className='section-wrap'>
     {props.children}
-    </div>
-    {
-      sectionLayoutStyle
-    }
+    <style jsx global>{`
+      .section-title {
+        margin-bottom: 64px;
+        font-size: 53px;
+        line-height: 1;
+        letter-spacing: -.03em;
+      }
+      .section-title span {
+        font-size: 24px;
+        line-height: 1;
+        margin-left: 16px;
+        opacity: .5;
+        letter-spacing: -.025em;
+      }
+      `}
+    </style>
   </section>
 )
 
