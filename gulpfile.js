e52e33f87489259da64c002b59d73953c786e477
Junyoung Choi <fluke8259@gmail.com>
2017-10-31 05:22:48 +0900

    Update gulpfile
    Discard unnecessary tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const path = require('path')
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
-const through = require('through2')
 
 // to get yaml data
 const unified = require('unified')
@@ -10,23 +9,26 @@ const remarkParse = require('remark-parse')
 const frontmatter = require('remark-frontmatter')
 const jsYaml = require('js-yaml')
 
-// template for devnotes
-const template = fs.readFileSync(`${__dirname}/templates/DevnotesTemplate.js`, 'utf8')
+const processor = unified()
+  .use(remarkParse)
+  .use(frontmatter, ['yaml'])
 
-// gulp tasks
-gulp.task('generate:md', () => {
-  gulp.src([`${__dirname}/data/posts/*.md`])
-    .pipe(gulpPlugin(devnotesProcessor, template))
-    .pipe(gulp.dest(`${__dirname}/pages/posts`))
-})
+// parseFrontmatter
+const parseFrontmatter = (input) => {
+const tree = processor.parse(input)
+if (tree.children && tree.children[0].type === 'yaml') {
+  return jsYaml.load(tree.children[0].value)
+}
+return {}
+}
 
-gulp.task('generate:list', () => {
-  const postYamls = fs.readdirSync('./data/posts')
+gulp.task('list', () => {
+  const postYamls = fs.readdirSync('./static/posts')
     .filter((postPath) => {
       return postPath.match(/\.md$/)
     })
     .map((postPath) => {
-      const postContent = fs.readFileSync(path.join('data/posts', postPath))
+      const postContent = fs.readFileSync(path.join('static/posts', postPath))
       return {
         ...parseFrontmatter(postContent),
         id: postPath.replace(/\.md$/, '')
@@ -36,7 +38,7 @@ gulp.task('generate:list', () => {
       ? -1
       : b.date.localeCompare(a.date)
     )
-  fs.writeFileSync('./data/generated/devnoteList.json', JSON.stringify(postYamls))
+  fs.writeFileSync('./static/generated/devnotes.json', JSON.stringify(postYamls))
 })
 
 gulp.task('imagemin', () => {
@@ -45,45 +47,6 @@ gulp.task('imagemin', () => {
     .pipe(gulp.dest(`${__dirname}/static/bashes`))
 })
 
-
-// gulpPlugin
-const gulpPlugin = (transformer, template) => {
-  return through.obj((file, encoding, callback) => {
-    file = transformer(file, encoding)
-
-    callback(null, file)
-  })
-}
-
-// devnotesProcessor
-const devnotesProcessor = (file, encoding) => {
-  const input = file.contents.toString(encoding)
-  const escapedInput = input
-    .replace(/`/g, '\\`')
-    .replace(/\${/, '\\${')
-  const output = template.replace(/\$source/, escapedInput)
-
-  file.contents = new Buffer(output)
-  file.path = file.path
-    .replace(/\.md$/, '.js')
-    .replace(/posts/, 'devnotes')
-
-  return file
-}
-
-const processor = unified()
-  .use(remarkParse)
-  .use(frontmatter, ['yaml'])
-
-// parseFrontmatter
-const parseFrontmatter = (input) => {
-  const tree = processor.parse(input)
-  if (tree.children && tree.children[0].type === 'yaml') {
-    return jsYaml.load(tree.children[0].value)
-  }
-  return {}
-}
-
 gulp.task('watch', () => {
-  gulp.watch('./data/posts/*.md', ['generate:md', 'generate:list'])
+  gulp.watch('./data/posts/*.md', ['generate:list'])
 })
