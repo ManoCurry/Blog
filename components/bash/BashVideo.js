7b93d774d508070b655851b85c2148721dd0e529
Jiseung Roh <jiseung.roh@gmail.com>
2017-11-03 00:26:37 +0900

    Update bash

--- components/bash/BashVideo.js
+++ components/bash/BashVideo.js
@@ -1,36 +1,65 @@
 import React from 'react'
 
-const BashVideo = ({
-  video
-}) => (
-  <div className='video-container'>
-    <iframe
-      src={video}
-      className='video'
-      frameBorder='0'
-      width='640'
-      height='400'
-      webkitallowfullscreen='true'
-      mozallowfullscreen='true'
-      allowFullScreen='true'>
-    </iframe>
-    <style jsx>{`
-      .video-container {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding-bottom: 62.3%;
-        margin-bottom: 10px;
-      }
-      .video-container iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-      }
-    `}</style>
-  </div>
-)
+class BashVideo extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      show: false
+    }
+  }
+
+  componentDidMount () {
+    this.setState({
+      show: true
+    })
+  }
+
+  renderIframe = (video) => (
+      `<iframe
+        src=${video}
+        class='video'
+        frameBorder='0'
+        width='320'
+        height='200'
+        style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;'
+        webkitallowfullscreen='true'
+        mozallowfullscreen='true'
+        allowFullScreen='true'>
+      </iframe>
+    `
+  )
+
+  render (props) {
+    const {
+      video
+    } = this.props
+
+    return (
+      <div className='media'
+        ref={video => this.video = video}>
+        {
+          this.state.show
+            ? <div className='video-container'
+              dangerouslySetInnerHTML={
+                {__html: this.renderIframe(video)}} />
+            : null
+        }
+        <style jsx>{`
+          .media {
+            width: 100%;
+          }
+          .video-container {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 62.3%;
+            margin-bottom: 10px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
 
 export default BashVideo
