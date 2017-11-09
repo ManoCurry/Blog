473d447f92bbb24887382d5746010d9656c62833
Jiseung Roh <jiseung.roh@gmail.com>
2017-11-09 20:06:56 +0900

    Add tags in the list

--- pages/devnotes/index.js
+++ pages/devnotes/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Link from 'next/link'
+import moment from 'moment'
 import AppLayout from '../../components/layouts/AppLayout'
 import devnotesData from '../../static/generated/devnotes'
+import { getDate } from '../../lib/helpers'
+
+const TagSpan = ({tag}) => (
+  <span>
+    {`#${tag}`}
+    <style jsx>{`
+      span {
+        margin-right: 10px;
+        font-size: 12px;
+        font-weight: 400;
+        color: #868E96;
+        text-transform: capitalize;
+      }
+    `}</style>
+  </span>
+)
 
 const DevNoteItem = ({
   title,
   id,
+  date,
+  tags,
   author
 }) => (
   <li>
@@ -18,7 +37,21 @@ const DevNoteItem = ({
     >
       <a>
         <h3>{title}</h3>
-        <p>{author}</p>
+        <p className='author'>
+          {
+            author === '준영'
+              ? <img src='/static/images/text-junyoung.svg' alt={author}/>
+              : <img src='/static/images/text-jiseung.svg' alt={author}/>
+          }
+        </p>
+        <p className='date'>{getDate(date)}</p>
+        <p className='tags'>
+          {
+            tags.map((item, index) => {
+              return <TagSpan key={index} tag={item}/>
+            })
+          }
+        </p>
       </a>
     </Link>
     <style jsx>{`
@@ -36,15 +69,29 @@ const DevNoteItem = ({
       p {
         font-size: 1rem;
         font-weight: 700;
-        line-height: 1;
+        line-height: 1.2;
+      }
+      p svg {
+        width: 20px;
+        height: auto;
+      }
+      .author {
+        margin-top: 16px;
+        margin-bottom: 8px;
+      }
+      .date {
+        font-size: 12px;
+        color: #868E96;
+        text-transform: uppercase;
       }
     `}</style>
   </li>
 )
 
+
 const DevNotesList = (props) => (
   <AppLayout>
-    <h2 className='section-title'>
+    <h2 className='section-title sr-only'>
       Devnotes
     </h2>
     <ul className='section-list'>
@@ -55,6 +102,8 @@ const DevNotesList = (props) => (
             title={devnote.title}
             id={devnote.id}
             author={devnote.author}
+            date={devnote.date}
+            tags={devnote.tags}
           />)
         )
       }
